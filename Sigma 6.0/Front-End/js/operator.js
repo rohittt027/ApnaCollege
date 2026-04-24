4245c57066e5fab53e26d7729f2f46ab1908d75d
rohittt027 <rohitkaithwaria99@gmail.com>
2026-04-24 15:33:19 +0530

    learning js

---
+++ Sigma 6.0/Front-End/js/operator.js
@@ -0,0 +1,53 @@
+// Arithmetic Operatos
+
+let a = 5;
+let b = 10;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a ** b);
+
+
+// Comparisional Operators
+
+let age = 18;
+
+console.log(age > 18)  //false
+console.log(age >= 18)  //true
+console.log(age < 18)  //false
+console.log(age <= 18) //true
+
+// NOTE:-
+3 > 5  //false 
+5 < 6  //true
+5 >= 5  //true
+0 < 5  //true
+0 < -2  //false
+0 < -99  //false
+5 == 5  //true 
+5!= 5  //false
+5!= 4  //true
+
+
+// NOTE:-
+// == :- compare value, not typeo
+// === :- compare type & value
+
+'123' == 123
+true
+1 == '1'
+true
+0 == ' '
+true
+'123' === 123
+false
+1 === '1'
+false
+null == undefined
+true
+null === undefined
+false
+
+
