2bb7c39b2ae7456c1ff4d35e90ff69e015a2345b
rohittt027 <rohitkaithwaria99@gmail.com>
2026-04-24 09:43:06 +0530

    templateLiterals

--- Sigma 6.0/Front-End/js/templateLeterals.js
+++ Sigma 6.0/Front-End/js/templateLeterals.js
@@ -1,7 +1,11 @@
 let pencilPrice = 10;
 let erasorPrice = 5;
 
-console.log("The total price is :", pencilPrice + erasorPrice, " Rupees.");
+// console.log("The total price is :", pencilPrice + erasorPrice, " Rupees.");
+
+let output = `The total price is : ${pencilPrice + erasorPrice} Rupees.`; 
+
+console.log(output);
 
 // ---------------------------------------------------------------------------------------------------
 
@@ -10,3 +14,5 @@ let markerPrice = 15;
 let product = "The total price is : " + (penPrice + markerPrice) + " Rupees.";
 
 console.log(product);
+
+console.log(`The total price is : ${penPrice + markerPrice} Rupees.`);
