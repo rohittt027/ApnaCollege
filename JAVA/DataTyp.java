b86adce543d45701215621ba68a9c5d556c6ffef
rohittt027 <rohitkaithwaria99@gmail.com>
2026-09-24 22:39:52 +0530

    data types

---
+++ JAVA/DataTyp.java
@@ -0,0 +1,23 @@
+public class DataTyp {
+    public static void main(String [] args){
+        byte b = 8;
+        System.out.println(b);
+
+        char ch = 'a';
+        System.out.println(ch);
+        
+        boolean var = false;
+        float price = 10.5;
+        int number = 25;
+        //long
+        //double
+        short n = 2540;
+
+    }
+    
+}
+
+
+// ---------------------------------------------------------------------------------------------------
+
+
