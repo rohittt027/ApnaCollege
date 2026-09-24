d9bb317edb33d65cc8e4ee88428215ae7072a2f7
rohittt027 <rohitkaithwaria99@gmail.com>
2026-09-24 07:04:42 +0530

    java basics prgm

---
+++ JAVA/JavaBasics.java
@@ -0,0 +1,31 @@
+// public class JavaBasics {
+//     public static void main(String [] args){
+//         System.out.println("Hello World!");
+//     }
+    
+// }
+
+
+// Output:- Hello World!
+
+
+
+// ---------------------------------------------------------------------------------------------------
+// public class JavaBasics {
+//     public static void main(String [] args){
+//         System.out.println("1 2 3 4 5");
+//     }
+// }
+
+// Output:- 1 2 3 4 5
+
+
+// ---------------------------------------------------------------------------------------------------
+// public class JavaBasics {
+//     public static void main(String [] args){
+//         System.out.print("Hello \nWorld");
+//     }
+// }
+
+// Output:- Hello 
+//          World
---
+++ JAVA/Ptrn.java
@@ -0,0 +1,8 @@
+public class Ptrn {
+ public static void main(String [] args){
+    System.out.println("****");
+    System.out.println("***");
+    System.out.println("**");
+    System.out.println("*");
+ }    
+}
