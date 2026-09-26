b8cd09d3190112abfb1cb6c32864d7b488d8077e
rohittt027 <rohitkaithwaria99@gmail.com>
2026-09-26 23:52:02 +0530

    type casting & conversion

---
+++ JAVA/TypeCasting.java
@@ -0,0 +1,46 @@
+// import java.util.Scanner;
+
+// public class TypeCasting {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         // float a = 25.9999f;
+//         // int b = (int) a;
+
+//         System.out.print(b);
+//     }
+    
+// }
+
+
+// ---------------------------------------------------------------------------------
+
+// import java.util.Scanner;
+
+// public class TypeCasting {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         float marks = 99.999f;
+//         int marks2 = (int) marks;
+//         System.out.print(marks2);
+//     }
+    
+// }
+ 
+
+// ---------------------------------------------------------------------------------
+
+// public class TypeCasting {
+//     public static void main(String[] args){
+//         char ch = 'a';
+//         char ch2 = 'b'; // 'c', 'd', 'e'
+//         int number = ch;
+//         int number2 = ch2;
+//         System.out.println(number);
+//         System.out.println(number2);
+
+//         // Output:-
+//         // number = 97
+//         // number2 = 98
+//     }
+    
+// }
