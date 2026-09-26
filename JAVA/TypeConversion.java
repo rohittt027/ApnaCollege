bc995b772553a6269dbb942ffa76212f5a044fc5
rohittt027 <rohitkaithwaria99@gmail.com>
2026-09-26 22:22:27 +0530

    varible & data types

---
+++ JAVA/TypeConversion.java
@@ -0,0 +1,30 @@
+// import java.util.*;
+
+// public class TypeConversion {
+//     public static void main(String[] args){
+//         int a = 25;
+//         long b = a;
+
+//         // long a = 25;|------> Conversion is not possible
+//         // int b = a;  |------> It gives an error
+
+
+//         System.out.println(b);
+//     }
+    
+    
+// }
+
+
+
+// -------------------------------------------------------------------------------
+
+// import java.util.*;
+// public class TypeConversion {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         // int number = sc.nextFloat(); ---> gives an error
+//         float number = sc.nextInt();
+//         System.out.println(number);
+//     }
+// }
